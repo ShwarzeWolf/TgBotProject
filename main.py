56b7557e7eaa6b98091c94d2d14f848217e53241
Kristina Sharaeva <kristinasharaeva1999@gmail.com>
2024-02-07 19:27:01 +0400

    Возвращаем сообщение об ошибке при недоступности сервера

--- main.py
+++ main.py
@@ -13,8 +13,12 @@ def greet_human(message):
 @bot.message_handler(commands=['get_weather'])
 def get_weather(message):
     url = f'https://api.openweathermap.org/data/2.5/weather?lat={40.178}&lon={40.505}&appid={settings.WEATHER_API_KEY}'
-    weather = requests.get(url).json()
-    bot.send_message(message.chat.id, weather["weather"][0]["description"])
+    response = requests.get(url)
+    if response.status_code == 200:
+        weather = response.json()
+        bot.send_message(message.chat.id, weather["weather"][0]["description"])
+    else:
+        bot.send_message(message.chat.id, 'Сервис определния погоды недоступен, пожалуйста, попробуйте позже')
 
 
 bot.polling()
