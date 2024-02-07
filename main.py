ba6b2b53d40bf0c4e1f7257993b3d13e930b3ca4
Kristina Sharaeva <kristinasharaeva1999@gmail.com>
2024-02-07 20:08:25 +0400

    Добавляем сохранение информации о городах пользователей

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ cities = {
     'Yerevan': Coordinates(40.178, 40.505),
 }
 
+users = {}  # всегда сбрасывается -> базы данных
+
 
 @bot.message_handler(commands=['start'])
 def greet_human(message):
@@ -20,20 +22,37 @@ def greet_human(message):
 
 @bot.message_handler(commands=['get_weather'])
 def get_weather(message):
-    coordinates = cities['Yerevan']
-    url = f'https://api.openweathermap.org/data/2.5/weather?lat={coordinates.latitude}&lon={coordinates.longitude}&appid={settings.WEATHER_API_KEY}'
-    response = requests.get(url)
-    if response.status_code == 200:
-        weather = response.json()
-        bot.send_message(message.chat.id, weather["weather"][0]["description"])
+    user_city = users.get(message.chat.id)
+
+    if user_city and user_city in cities:
+        coordinates = cities[user_city]
+        url = f'https://api.openweathermap.org/data/2.5/weather?lat={coordinates.latitude}&lon={coordinates.longitude}&appid={settings.WEATHER_API_KEY}'
+        response = requests.get(url)
+        if response.status_code == 200:
+            weather = response.json()
+            bot.send_message(message.chat.id, weather["weather"][0]["description"])
+        else:
+            bot.send_message(message.chat.id, 'Сервис определния погоды недоступен, пожалуйста, попробуйте позже')
     else:
-        bot.send_message(message.chat.id, 'Сервис определния погоды недоступен, пожалуйста, попробуйте позже')
+        bot.send_message(message.chat.id, 'Нет данных для определения погоды. Пожалуйста, выполните настрйоку через команду setup или '
+                                          'подождите пока ваш город будет добавлен в словарь ')
 
 
 @bot.message_handler(commands=['get_fox'])
 def get_fox(message):
     url = f'https://randomfox.ca/images/{random.randint(1, 100)}.jpg'
     bot.send_photo(message.chat.id, url)
+
+
+@bot.message_handler(commands=['setup'])
+def ask_city(message):
+    bot.send_message(message.chat.id, 'В каком городе вы живете?')
+    bot.register_next_step_handler(message, proceed_city)
+
+
+def proceed_city(message):
+    bot.send_message(message.chat.id, f'Какой хороший город - {message.text}!')
+    users[message.chat.id] = message.text
 
 
 bot.polling()
